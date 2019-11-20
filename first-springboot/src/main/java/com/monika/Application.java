d37f6e9d664b1f25e98b39f2953f2a460452c60f
monika shetty <monikacshetty@gmail.com>
2019-11-20 17:02:59 +0530

    Renaming Project and Package Structure

--- first-springboot/src/main/java/com/monika/Application.java
+++ first-springboot/src/main/java/com/monika/Application.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package com.monika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Task2Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Task2Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
--- first-springboot/src/test/java/com/monika/ApplicationTests.java
+++ first-springboot/src/test/java/com/monika/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.monika;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Task2ApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
